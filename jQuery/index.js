a3c26bc395671de546f60d08ab0433c77e12a73a
Himanshu Malik <jsm.28415@gmail.com>
2021-06-01 19:01:23 +0530

    jQuery_Learned

---
+++ jQuery/index.js
@@ -0,0 +1,17 @@
+
+
+$(document).keypress("click", function(event) {
+  $("h1").animate({opacity: 0.5});
+})
+
+
+// hide the contentr which u want
+// $(document).keypress("click", function(event) {
+//   $("h1").hide();
+// })
+
+
+// key change listner
+// $(document).keypress(function(event) {
+//   $("h1").text(event.key);
+// })
